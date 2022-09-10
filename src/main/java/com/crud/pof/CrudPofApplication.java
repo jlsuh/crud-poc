1d19504299818f9723dc203ba1793f4533195635
jlsuh <joelleonardosuh@gmail.com>
2022-09-10 17:20:04 -0300

    chore: enable web mvc

--- src/main/java/com/crud/pof/CrudPofApplication.java
+++ src/main/java/com/crud/pof/CrudPofApplication.java
@@ -1,19 +1,18 @@
 package com.crud.pof;
 
-import com.crud.pof.model.Person;
 import com.crud.pof.repository.PersonRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class CrudPofApplication {
-
     public static void main(String[] args) {
-        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(CrudPofApplication.class, args);
-        PersonRepository personRepository = configurableApplicationContext.getBean(PersonRepository.class);
-        // Person person = new Person("Cris", "Liu", Byte.valueOf("23"));
-        // personRepository.save(person);
+        ConfigurableApplicationContext configurableApplicationContext =
+                SpringApplication.run(CrudPofApplication.class, args);
+        PersonRepository personRepository =
+                configurableApplicationContext.getBean(PersonRepository.class);
     }
-
 }
