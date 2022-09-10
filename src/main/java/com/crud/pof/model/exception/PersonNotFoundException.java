8325a06db25312a25d48d2115b2651e3a5b34c1b
jlsuh <joelleonardosuh@gmail.com>
2022-09-10 16:12:33 -0300

    refactor: id as final

--- src/main/java/com/crud/pof/model/exception/PersonNotFoundException.java
+++ src/main/java/com/crud/pof/model/exception/PersonNotFoundException.java
@@ -3,7 +3,7 @@ package com.crud.pof.model.exception;
 import java.text.MessageFormat;
 
 public class PersonNotFoundException extends RuntimeException {
-    public PersonNotFoundException(Long id) {
+    public PersonNotFoundException(final Long id) {
         super(MessageFormat.format("Person with [ID: {0}] not found", id));
     }
 }
