90901446e0350d3e494c5d0386140c689b1e5bba
jlsuh <joelleonardosuh@gmail.com>
2022-09-11 01:36:11 -0300

    refactor: define routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Bar from './components/Bar';
 import FooterComponent from './components/Footer';
+import Table from './components/Table';
 
 function App() {
   return (
     <div>
-      <Bar />
-      <div className='container'>
-      </div>
-      <FooterComponent />
+      <Router>
+        <Bar />
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' element={<Table />} />
+            <Route exact path='/persons' element={<Table />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
