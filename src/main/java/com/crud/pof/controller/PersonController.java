870b48d302485b4b5ec37de6189f09cb12326515
jlsuh <joelleonardosuh@gmail.com>
2022-09-11 00:45:27 -0300

    chore: add cross origin

--- src/main/java/com/crud/pof/controller/PersonController.java
+++ src/main/java/com/crud/pof/controller/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
