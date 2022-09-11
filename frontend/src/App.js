1be081d153c504f71057d9e521ebb7c7eca006a9
jlsuh <joelleonardosuh@gmail.com>
2022-09-11 20:23:29 -0300

    feat: edit person route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Bar from './components/Bar';
 import FooterComponent from './components/Footer';
 import Table from './components/Table';
 import CreatePerson from './components/CreatePerson';
+import UpdatePerson from './components/UpdatePerson';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path='/' element={<Table />} />
             <Route path='/persons' element={<Table />} />
             <Route path='/persons/new' element={<CreatePerson />} />
+            <Route path='/persons/edit/:id' element={<UpdatePerson />} />
           </Routes>
         </div>
         <FooterComponent />
