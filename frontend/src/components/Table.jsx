b81a8614916e53c67ed41fc6ed9fd0b97bf6ec99
jlsuh <joelleonardosuh@gmail.com>
2022-09-11 12:57:48 -0300

    refactor: table as functional component

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,52 +1,40 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import PersonAPI from '../api/PersonAPI'
 
-class Table extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      persons: []
-    }
-  }
-  componentDidMount() {
-    PersonAPI.getPersons().then((res) => {
-      this.setState({ persons: res.data })
-    })
-  }
-  render() {
-    return (
-      <Fragment>
-        <h2 className='text-center'>Personas</h2>
-        <div className='row'>
-          <table className='table table-striped table-bordered'>
-            <thead>
-              <tr>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Edad</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                this.state.persons.map(
-                  person =>
-                    <tr key={person.id}>
-                      <td>{person.firstName}</td>
-                      <td>{person.lastName}</td>
-                      <td>{person.age}</td>
-                      <td>
-                        Acción
-                      </td>
-                    </tr>
-                )
-              }
-            </tbody>
-          </table>
-        </div>
-      </Fragment>
-    );
-  }
-}
+export default function Table() {
+  const [persons, setPersons] = useState([])
 
-export default Table;
+  useEffect(() => {
+    PersonAPI.getPersons().then((res) => {
+      setPersons(res.data)
+    }, [])
+  }, [])
+
+  return (
+    <Fragment>
+      <h2 className='text-center'>Personas</h2>
+      <div className='row'>
+        <table className='table table-striped table-bordered'>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Edad</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {persons.map(person =>
+              <tr key={person.id}>
+                <td>{person.firstName}</td>
+                <td>{person.lastName}</td>
+                <td>{person.age}</td>
+                <td>Acción</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </Fragment>
+  );
+}
